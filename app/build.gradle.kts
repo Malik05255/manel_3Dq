4f97e022d6a0d7e0c3ecbfecc77bc1a19a5a0ec7
Malik05255 <mmbbrr99@gmail.com>
2026-09-12 19:05:53 +0300

    Bump Manzili HAI to 0.60.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.manzili.hai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 50
-        versionName = "0.50.0"
+        versionCode = 60
+        versionName = "0.60.0"
     }
 
     if (releaseSigningReady) {
