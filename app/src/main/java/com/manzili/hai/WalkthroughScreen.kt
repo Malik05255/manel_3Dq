85f0fc8867bcf476e637394f02f1e58f350c53f4
Malik05255 <mmbbrr99@gmail.com>
2026-09-12 18:09:18 +0300

    Fix walkthrough horizontal scrolling import

--- app/src/main/java/com/manzili/hai/WalkthroughScreen.kt
+++ app/src/main/java/com/manzili/hai/WalkthroughScreen.kt
@@ -1,6 +1,7 @@
 package com.manzili.hai
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
