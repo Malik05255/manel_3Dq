7847ea2c487b45fd84a90b0b74c68cc4a3a24321
Malik05255 <mmbbrr99@gmail.com>
2026-09-12 11:17:02 +0300

    Bump multi-floor solver optional Saudi rules release to 0.18.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.manzili.hai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "0.17.0"
+        versionCode = 18
+        versionName = "0.18.0"
     }
 
     compileOptions {
