26c0c96b74d8cfff756633bbbe73a3f0853abc26
Malik05255 <mmbbrr99@gmail.com>
2026-09-12 11:34:41 +0300

    Add unit-test dependencies for engine regression suite

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,5 +37,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.google.mlkit:text-recognition:16.0.1")
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.json:json:20240303")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
