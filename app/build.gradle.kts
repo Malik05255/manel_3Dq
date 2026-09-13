77cc93c025f65d2c4fdbac86148c556aa8e222d4
Malik05255 <mmbbrr99@gmail.com>
2026-09-13 13:17:31 +0300

    Bump hardened release to 0.62.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.manzili.hai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 61
-        versionName = "0.61.0"
+        versionCode = 62
+        versionName = "0.62.0"
     }
 
     if (releaseSigningReady) {
